5573ae4288896074722af386c36234c3a4a592d4
Robert Carter <racarter1215@gmail.com>
2020-07-16 17:04:53 -0700

    got all of the neighborhoods to show

---
+++ Lab09-LINQ/Lab09-LINQ/classes/GeoLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab09_LINQ.classes
+{
+    public class GeoLocation
+    {
+        public string Type { get; set; }
+        public List<double> Coordinates { get; set; }
+    }
+}
---
+++ Lab09-LINQ/Lab09-LINQ/classes/Properties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lab09_LINQ
+{
+    public class Properties
+    {
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Address { get; set; }
+        public string Borough { get; set; }
+        public string neighborhood { get; set; }
+        public string County { get; set; }
+    }
+}
